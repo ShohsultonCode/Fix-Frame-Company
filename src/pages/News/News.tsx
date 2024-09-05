0edcdebc0a2acabfab62cd9957b1c3a143097f67
Shohsulton Ummatov <shohsulton.198@gmail.com>
2024-09-05 23:30:55 +0500

    add: last commits

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -20,10 +20,10 @@ import {
   newsHouseImage13,
   newsHouseImage14,
   newsHouseImage15,
-  newsHouseImage16,
-  newsHouseImage17,
-  newsHouseImage18,
-  newsHouseImage19,
+  // newsHouseImage16,
+  // newsHouseImage17,
+  // newsHouseImage18,
+  // newsHouseImage19,
   newsHouseImage2,
   newsHouseImage3,
   newsHouseImage4,
@@ -158,10 +158,34 @@ const News = () => {
       ],
       title: "Building Your Dream Bathroom",
       text: `
-    The bathroom is one of the most used rooms in your home. It's a place where you can relax after a long day. Updating your bathroom or adding a new one is a great way to increase the value of your home. The Fix&Frame team can help you plan, design, and coordinate the perfect bathroom remodel that fits your budget.
+         The bathroom is one of the most used rooms in your home. It's a place where you can relax after a long day. Updating your bathroom or adding a new one is a great way to increase the value of your home. The Fix&Frame team can help you plan, design, and coordinate the perfect bathroom remodel that fits your budget.
       `,
     },
-//44
+    // {
+    //   id: 5,
+    //   images: [
+    //     {
+    //       id: 1,
+    //       image: newsHouseImage16,
+    //     },
+    //     {
+    //       id: 2,
+    //       image: newsHouseImage17,
+    //     },
+    //     {
+    //       id: 3,
+    //       image: newsHouseImage18,
+    //     },
+    //     {
+    //       id: 4,
+    //       image: newsHouseImage19,
+    //     },
+    //   ],
+    //   title: "Landscape and Snow Removal",
+    //   text: `
+    //     Welcome to Fix&Frame , your trusted partner for all your landscaping and snow removal needs. We believe that your outdoor spaces deserve the same attention to detail and care as your indoor spaces. Whether it’s creating a lush, vibrant landscape or ensuring safe and snow-free pathways during winter, Ninja Group Inc is here to make your outdoor dreams a reality.
+    //   `,
+    // },
   ];
 
   const arrowLeftBtnRef = useRef<HTMLButtonElement | null>(null);
